24b4c7e59edbcd7b378523524291648741552252
Kris Adiwinata <krisadiwinata12@gmail.com>
2025-12-23 13:11:09 +0700

    feat: Adding react toolkit lib and implement the dock UI component

--- app/components/Dock.tsx
+++ app/components/Dock.tsx
@@ -1,7 +1,26 @@
+'use client'
+import { useRef } from "react"
+import { dockApps } from "../constants"
+import { Tooltip } from "react-tooltip"
+
 const Dock = () => {
+  const dockRef = useRef<HTMLDivElement>(null)
+
+  const toggleApp = ({ id, canOpen }: { id: string, canOpen: boolean }) => {
+
+  }
   return (
     <section id="dock">
-        <h1>Hello Doc</h1>
+      <div ref={dockRef} className="dock-container">
+        {dockApps.map(({ id, name, icon, canOpen }) => (
+          <div key={id} className="relative flex justify-center">
+            <button type="button" className="dock-icon" aria-label={name} data-tooltip-id="dock-tooltip" data-tooltip-content={name} data-tooltiop-delay-show={150} disabled={!canOpen} onClick={() => toggleApp({ id, canOpen })}>
+              <img src={`/images/${icon}`} alt={name} loading="lazy" className={canOpen ? "" : "opacity-60"} />
+            </button>
+          </div>
+        ))}
+      </div>
+      <Tooltip id="dock-tooltip" place="top" className="tooltip" />
     </section>
   )
 }
