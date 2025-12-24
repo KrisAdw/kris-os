d348a9a78e0e85fda0dc0d008f19676425435b71
Kris Adiwinata <krisadiwinata12@gmail.com>
2025-12-24 13:22:56 +0700

    Refactor Dock component for performance and update window store

--- app/store/window.ts
+++ app/store/window.ts
@@ -2,26 +2,26 @@ import { create } from "zustand"
 import { immer } from "zustand/middleware/immer"
 import { INITIAL_Z_INDEX, WINDOW_CONFIG } from "../constants"
 
-type WindowConfigType = typeof WINDOW_CONFIG
-type WindowKey = keyof WindowConfigType
+export type WindowConfigType = typeof WINDOW_CONFIG
+export type WindowKey = keyof WindowConfigType
 
-interface WindowState {
-    window: WindowConfigType
+export interface WindowState {
+    windows: WindowConfigType
     nextZIndex: number
 }
 
-interface WindowActions {
+export interface WindowActions {
     openWindow: (windowKey: WindowKey, data?: any) => void
     closeWindow: (windowKey: WindowKey) => void
     focusWindow: (windowKey: WindowKey) => void
 }
 
 const useWindowStore = create<WindowState & WindowActions>()(immer((set) => ({
-    window: WINDOW_CONFIG,
+    windows: WINDOW_CONFIG,
     nextZIndex: INITIAL_Z_INDEX + 1,
 
     openWindow: (windowKey: WindowKey, data = null) => set((state) => {
-        const win = state.window[windowKey]
+        const win = state.windows[windowKey]
         win.isOpen = true
         win.zIndex = state.nextZIndex
         win.data = data ?? win.data
@@ -29,14 +29,16 @@ const useWindowStore = create<WindowState & WindowActions>()(immer((set) => ({
     }),
 
     closeWindow: (windowKey: WindowKey) => set((state) => {
-        const win = state.window[windowKey]
+        const win = state.windows[windowKey]
         win.isOpen = false
         win.zIndex = INITIAL_Z_INDEX
         win.data = null
     }),
 
     focusWindow: (windowKey) => set((state) => {
-        const win = state.window[windowKey]
+        const win = state.windows[windowKey]
         win.zIndex = state.nextZIndex++
     })
 })))
+
+export default useWindowStore
